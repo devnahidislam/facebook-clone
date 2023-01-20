0c1d9924a0402d3d582ed7c49d1672e83505b3f6
devnahidislam <mdnahidislam30.com>
2023-01-20 23:58:58 +0600

    Post publish function created.

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,18 +1,43 @@
-import jwt from 'jsonwebtoken';
-import { db } from '../db.js';
+import jwt from "jsonwebtoken";
+import moment from "moment/moment.js";
+import { db } from "../db.js";
 
 export const getPosts = (req, res) => {
   const token = req.cookies.AccessToken;
-  if (!token) return res.status(401).json('You are not Loged in.');
+  if (!token) return res.status(401).json("You are not Loged in.");
 
   jwt.verify(token, process.env.KEY, (err, userInfo) => {
-    if (err) return res.status(403).json('Invalid User.');
+    if (err) return res.status(403).json("Invalid User.");
 
     const q = `SELECT p.*, firstname, surname, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) LEFT JOIN relationship AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId = ?  OR p.userId = ? ORDER BY p.createdAt DESC;`;
 
     db.query(q, [userInfo.id, userInfo.id], (err, data) => {
       if (err) return res.status(500).json(err);
       return res.status(200).json(data);
+    });
+  });
+};
+
+export const addPost = (req, res) => {
+  const token = req.cookies.AccessToken;
+  if (!token) return res.status(401).json("You are not Loged in.");
+
+  jwt.verify(token, process.env.KEY, (err, userInfo) => {
+    if (err) return res.status(403).json("Invalid User.");
+
+    const q =
+      "INSERT INTO posts (`desc`,`img`,`createdAt`,`userId`) VALUES (?);";
+
+    const values = [
+      req.body.desc,
+      req.body.img,
+      moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+      userInfo.id,
+    ];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("Post has been created.");
     });
   });
 };
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,8 +1,9 @@
-import express from 'express';
-import { getPosts } from '../controllers/posts.js';
+import express from "express";
+import { getPosts, addPost } from "../controllers/posts.js";
 
 const router = express.Router();
 
-router.get('/', getPosts);
+router.get("/", getPosts);
+router.post("/addpost", addPost);
 
 export default router;
